c36b9bb61a828f409efa53ab59ce181c3f966950
ziakha12 <kzzia06@gmail.com>
2023-11-14 21:55:26 +0500

    feat: react_router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,50 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './componenets/Layout';
+import Home from './componenets/Home/Home';
+import About from './componenets/About/About';
+import Contact from './componenets/Contact/Contact';
+import User from './componenets/User/User';
+import Github from './componenets/Github/Github';
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Layout/>,
+    children : [
+      {
+        path : "",
+        element : <Home/>
+
+      },
+      {
+        path : "about",
+        element : <About/>
+
+      },
+      {
+        path : "contact",
+        element : <Contact/>
+      },
+      {
+        path : "user/:userid",
+        element: <User/>
+      },
+      {
+        path : "github",
+        element: <Github/>
+      },
+    ]
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
